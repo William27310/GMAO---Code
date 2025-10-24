faedd8356d80859f20b6f7c41ba0b4304f4d7094
William27310 <william.27@free.fr>
2025-10-24 11:38:04 +0200

    GMAO Formatage de la page sous-équipement via la page équipement

--- src/Views/sous_equipement.php
+++ src/Views/sous_equipement.php
@@ -29,30 +29,99 @@
                         <a href="index.php?url=machine" class="mt-5 btn btn-outline-dark">Revenir en arrière</a>
                     </div>
                     <hr class="mt-3">
-                </div>
 
-                <div class="d-flex flex-column flex-md-row justify-content-around mb-5">
+                    <div class="d-flex justify-content-around">
+                        <a href="index.php?url=create_machine" class="btn btn-outline-dark">Renseigner un nouveau équipement</a>
+                        <a href="" class="btn btn-outline-dark">Modifier le renseignement</a>
+                        <a href="" class="btn btn-outline-dark">Supprimer le renseignement</a>
+                    </div>
+                    <hr>
 
-                    <div class="right_panel_two card d-flex justify-content-center order-1 order-md-2 mt-2 p-5 mb-2">
-                        <h3 class="text-center mb-5">Equipement :</h3>
-                        <h4 class="text-center mb-5">Sous-équipement</h4>
-                        <div class="d-flex justify-content-around mt-4">
-                            <p class="mt-3">Se-Nom <br>
-                                Se-fonction <br>
-                                Se-type
+                    <!-- Multiple Toggle -->
+                    <p class="d-inline-flex gap-1">
+                        <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1">MTBF</button>
+                        <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">MTTR</button>
+                        <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample3" aria-expanded="false" aria-controls="multiCollapseExample3">Pareto</button>
+                        <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample4" aria-expanded="false" aria-controls="multiCollapseExample4">TRS</button>
+                        <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2 multiCollapseExample3 multiCollapseExample4">Toutes les images</button>
+                    </p>
+
+                    <div class="row g-3 mt-2">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="collapse multi-collapse" id="multiCollapseExample1">
+                                <div class="card card-body text-center">
+                                    <img src="upload/afpa.png" alt="Image 1" class="img-fluid rounded">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6">
+                            <div class="collapse multi-collapse" id="multiCollapseExample2">
+                                <div class="card card-body text-center">
+                                    <img src="upload/afpa.png" alt="Image 2" class="img-fluid rounded">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6">
+                            <div class="collapse multi-collapse" id="multiCollapseExample3">
+                                <div class="card card-body text-center">
+                                    <img src="upload/afpa.png" alt="Image 3" class="img-fluid rounded">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6">
+                            <div class="collapse multi-collapse" id="multiCollapseExample4">
+                                <div class="card card-body text-center">
+                                    <img src="upload/afpa.png" alt="Image 4" class="img-fluid rounded">
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                    </div>
+
+                    <!-- Equipement -->
+                    <div class="d-flex justify-content-around">
+                        <div class="card col-8 col-sm-4 col-md-2 mb-3">
+                            <img src="upload/afpa.png" class="card-img-top" data-bs-toggle="modal" data-bs-target="#imageModal" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Sous-équipement</h5>
+                                <p>Nom <br>
+                                    Fonctionnalité <br>
+                                    Type</p>
+                            </div>
                         </div>
                     </div>
 
-                    
-                    <div>
-                        <img src="upload/schéma_test2.png" class="image_se border border-dark img-fluid" alt="">
+
+                    <!-- Bouton -->
+                    <div class="d-flex justify-content-around mb-3">
+                        <a href="index.php?url=intervention" class="btn btn-outline-dark">Créer une intervention</a>
+                        <a href="" class="btn btn-outline-dark">Renseigner une intervention</a>
+                        <a href="" class="btn btn-outline-dark">Historique des intervention</a>
                     </div>
                 </div>
 
             </div>
         </div>
     </div>
+
+    <!-- Modal Image Cliquable -->
+    <div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-body p-0">
+                    <img src="upload/afpa.png" alt="Image agrandie" class="img-fluid w-100">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                </div>
+            </div>
+        </div>
     </div>
+    <!-- Modal -->
+
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
